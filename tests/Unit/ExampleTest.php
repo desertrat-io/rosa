4e9e19f7079229c716a8951b11e2f72d4cfaaf05
djzara <mlawson1986@gmail.com>
2019-04-15 18:42:25 -0500

    travis setup

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace RosaTest\Unit;
+
+use RosaTest\RosaBaseTest;
+
+/**
+ * Class ExampleTest
+ * @package RosaTest\Unit
+ */
+class ExampleTest extends RosaBaseTest
+{
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testExample() : void
+    {
+        $this->assertTrue(true);
+    }
+}
