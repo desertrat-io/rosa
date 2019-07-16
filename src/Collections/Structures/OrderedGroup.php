6a614d7206eeefd3f99751f7c1670d267fc967ee
Michael Lawson <mlawson1986@gmail.com>
2019-07-15 20:48:00 -0500

    fronds-12 base ordered interface created

--- src/Collections/Structures/OrderedGroup.php
+++ src/Collections/Structures/OrderedGroup.php
@@ -15,19 +15,27 @@ interface OrderedGroup
      * Useful if the group mutates in some way that affects ordering
      * @param string $key
      */
-    public function orderByKey(string $key) : void;
+    public function orderByKey(string $key): void;
 
     /**
      * Was the order changed between the last access of the Group store?
      * This can be used for integrity checks of the group
      * @return bool
      */
-    public function orderChanged() : bool;
+    public function orderChanged(): bool;
 
     /**
      * @return string
      */
-    public function orderCursorLocation() : string;
+    public function orderCursorLocation(): string;
+
+    /**
+     * Set the cursor to point to a new element in the ordered collection. This
+     * is to more strictly enforce ordering with these objects instead of just relying on
+     * the @see \Iterator interface.
+     * @param int $newLocation
+     */
+    public function setOrderCursor(int $newLocation): void;
 
     /**
      * Add an item to the end of the collection.
@@ -46,8 +54,50 @@ interface OrderedGroup
 
     /**
      * Received an unordered group, then creates a new ordered group according to the given closure
+     * That closure will determine the ordering to see the group with.
      * @param Closure $orderingCallback
      * @return OrderedGroup
      */
     public function orderedMap(Closure $orderingCallback): OrderedGroup;
+
+    /**
+     * Perform a filter operation over the group that is guaranteed to be in order
+     * @param Closure $orderingCallback
+     * @return OrderedGroup
+     */
+    public function orderedFilter(Closure $orderingCallback): OrderedGroup;
+
+
+    /**
+     * Create an exact copy of the originating group, but reorder it
+     * based on the given callback. This allows two identical datasets to be maintained
+     * and managed in different orders.
+     * @param Closure $orderingCallback
+     * @return OrderedGroup
+     */
+    public function reorderAndClone(Closure $orderingCallback): OrderedGroup;
+
+    /**
+     * Perform a union of all passed in OrderedGroups that maintains the order
+     * defined in the first group
+     * @param OrderedGroup ...$orderedGroups
+     * @return OrderedGroup
+     */
+    public function union(OrderedGroup ...$orderedGroups): OrderedGroup;
+
+    /**
+     * Perform an intersection of all passed in OrderedGroups that maintains the order
+     * defined in the first group
+     * @param OrderedGroup ...$orderedGroups
+     * @return OrderedGroup
+     */
+    public function intersect(OrderedGroup ...$orderedGroups): OrderedGroup;
+
+    /**
+     * Reset the cursor to the beginning of the group for linear groups, and to the root
+     * for non linear
+     */
+    public function resetCursor(): void;
+
+
 }
